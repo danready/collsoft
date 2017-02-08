fd6825a1567f341cd3de803ff0b4ced1df6a1558
Mirco Andreotti <mandreot@elicolle.fe.infn.it>
2017-02-08 09:46:25 +0100

    commit Server Ufficiale

--- ExpertGUI/bin/.ui/mainwindow.h
+++
@@ -1,8 +0,0 @@
-/****************************************************************************
-** Form interface generated from reading ui file 'mainwindow.ui'
-**
-** Created by User Interface Compiler
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
-
